145c5b211ee183e15abd2dd942e7e01fcb6f6bf5
tu <tu0703@gmail.com>
2017-06-25 22:56:44 +0700

    update to v1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='get_fshare',
-      version='1.0.0',
+      version='1.0.1',
       description='Python lib to get link Fshare.vn',
       url='http://github.com/tudoanh/get_fshare',
       author='Do Anh Tu',
