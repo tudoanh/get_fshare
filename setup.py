4ebbbab792e1c57078454abc35b20da78ab106c0
Do Anh Tu <tu0703@gmail.com>
2019-02-12 21:59:19 +0700

    Update docs

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='get_fshare',
-    version='2.3.0',
+    version='2.3.1',
     description='Python lib to get link Fshare.vn',
     url='http://github.com/tudoanh/get_fshare',
     author='Do Anh Tu',
     author_email='tu0703@gmail.com',
     license='MIT',
-    install_requires=['requests>=2.18.1', 'lxml>=3.8.0'],
     packages=['get_fshare'],
     zip_safe=False,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     python_requires='>=3.5',
 )
